97363d91bffbfc71feb206ec8b413afe28142b2c
ozodbek <nozodbek572@gmail.com>
2024-10-15 14:02:47 +0500

    done

---
+++ main.js
@@ -0,0 +1,180 @@
+
+
+
+// 1-MISOL 
+
+// let uzunlik = prompt ("uzunlikni kiriinting");
+// let sm = parseFloat(uzunlik)
+// let result = parseInt(sm/100);
+// alert(`${result} m`)
+
+// 1-MISOL TUGASHI
+
+
+
+
+
+// 2-MISOL 
+
+
+// let ogirlik = prompt("ogirlikni kiriting");
+// let kg = parseFloat(ogirlik);
+// let result = parseInt(kg/1000 );
+// alert(`${result} t`);
+
+// 2-MISOL tugashi
+
+
+
+
+// 3-MISOL 
+
+
+// let hajmi = prompt("hajmini kiriting");
+// let kb = parseFloat(hajmi); 
+// let result = parseInt(kb/1024);
+// alert(`${result} kb`);
+
+// 3-MISOL tugashi 
+
+
+
+
+// 4 va 5-chi misol 
+
+// let a =prompt("a  kiriting");
+// let b = prompt("b kiriting");
+
+// a = parseFloat(a)
+// b = parseFloat(b)
+
+// let result = (a%b)
+
+// alert(`a bolmesi b eguvde ${result} ga teng`);
+
+// let a =prompt("a  kiriting");
+// let b = prompt("b kiriting");
+
+// a = parseFloat(a)
+// b = parseFloat(b)
+
+// let result = (a/b)
+
+// alert(`a bolmesi b eguvde ${result} ga teng`);
+
+// 4va 5-chi misol tugashi
+
+
+
+
+// 6-chi misol
+
+// let a = prompt("ikki xonali son kiriting");
+
+// let onlik = parseInt(a/10)
+// let birlik = (a%10)
+
+// alert (onlik +  "onlik");
+
+
+// alert onlar va birlar xonasidagi son (birlik + "birlik");
+
+// 6-chi misol tugashi
+
+
+
+
+// 7-misol 
+
+// let a = prompt("ikki xonali son kiriting");
+
+// let onlik = parseInt(a/10)
+// let birlik = (a%10)
+
+// alert (`ikki conali son yigindisi ${birlik + onlik}`);
+
+
+// 7-misol tugashi
+
+
+
+
+// 8-misol 
+
+// let a = prompt("ikki xonali son kiriting");
+
+// let onlik = parseInt(a/10)
+// let birlik = (a%10)
+
+// alert (`orni almashganda hosil bolgan son ${birlik}, ${onlik}`);
+
+// 8-misol tugashi
+
+
+
+
+// 9-misol 
+
+// let a = prompt("uch xonali son kiriting");
+
+// let yuzlik = parseInt(a/100)
+
+// alert ( ` yuzlar xonasidagi son ${yuzlik} `);
+
+// 9-misol tugashi
+
+
+
+// 10-misol
+
+// let a = prompt("uch xonali son kiriting");
+
+// let onlik = parseInt(a/10%10)
+// let birlik=a%10;
+// alert ( `birlik son ${birlik} onlik son ${onlik} ` )
+
+// 10-misol tugashi
+
+
+
+
+// 11-misol
+
+// let a = prompt("uch xonali son kiriting");
+
+// let yuzlik = parseInt(a/100)
+// let onlik = parseInt(a/10%10)
+// let birlik = a%10
+
+// alert (`hammasining yigindisi${birlik + onlik + yuzlik} `);
+
+// 11-misol tugashi
+
+
+
+// 12-misol
+
+// let a = prompt("Uch xonali son kiriting"); 
+
+// let yuzlik = parseInt(a / 100);
+// let onlik = parseInt((a / 10) % 10); 
+// let birlik = a % 10; 
+
+// alert(`Teskari tartibda: ${birlik}${onlik}${yuzlik}`);
+
+// 12-misol tugashi
+
+
+
+
+// 13-misol 
+
+// let a = +prompt("uch xonali son kiriting");
+// let b = parseInt(a/100)
+// let c = a % 100*10;
+// alert ( `Raqamlar alishtirilgandan keyin ${c + b}`);
+
+// 13-misol tugashi
+
+
+// bu yerda paxta qoyilgan bolishi munkun edi 😂
